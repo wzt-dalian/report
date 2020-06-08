ebe01021b8047eb3e9b542df092e897cd5f2d842
wzt-dalian <wzt_dalian@163.com>
2020-06-08 14:38:38 +0800

    my test

---
+++ guess.py
@@ -0,0 +1,23 @@
+from random import randint
+
+def play():
+    rand_int = randint(0,1000)
+    print(rand_int)
+
+    while True:
+        intnumber = int(input("please input a number:"))
+
+        if intnumber == rand_int:
+            print(f"you found the number ({rand_int}), congratulation!")
+            break
+
+        elif intnumber < rand_int:
+            print("the number you input is less than the number we guess.")
+            continue
+
+        elif intnumber > rand_int:
+            print("the number you input is more than the number we guess.")
+            continue
+
+if __name__ == "__main__":
+    play()
