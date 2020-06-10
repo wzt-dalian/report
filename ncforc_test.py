3f47224b5f9ea747168a119d208594f787ee0cb7
wzt-dalian <wzt_dalian@163.com>
2020-06-10 15:54:19 +0800

    ncforctest

--- ncforc_test.py
+++ ncforc_test.py
@@ -8,7 +8,7 @@ cwd = os.getcwd()
 data_path = os.path.join(cwd, "binout0000")
 br = BinoutReader(data_path)
 nid = 1291
-contact_id = 62
+contact_id = 629
 time =         br.get_data(bdt.BINOUT_NCFORC_X, cid=contact_id)
 num_timestep = br.get_data(bdt.BINOUT_NCFORC_NUM_TIMESTEP, cid=contact_id)
 mydict = {}
